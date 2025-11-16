bfd86ba042f8e76612ddefb0979914f6910d7bb4
Caua Mattos <caua.h4k@gmail.com>
2025-11-16 17:29:23 -0300

    feat: writing first test

---
+++ src/test/java/com/beyond/beyond/integration/ActivitiesIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.beyond.beyond.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ActivitiesIntegrationTest {
+    @Test
+    void shouldCreateAnRewardableActivitie() {
+        
+    }
+}
