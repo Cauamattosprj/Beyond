56beb8fa7f29f509aa04802149769cd81ec0a864
Caua Mattos <caua.h4k@gmail.com>
2025-11-18 22:46:54 -0300

    feat: implement activity mapper and toentity method

---
+++ src/main/java/com/beyond/beyond/mappers/ActivityMapper.java
@@ -0,0 +1,51 @@
+package com.beyond.beyond.mappers;
+
+
+
+import org.springframework.stereotype.Component;
+
+import com.beyond.beyond.dto.ActivityDTO;
+import com.beyond.beyond.models.Activity;
+import com.beyond.beyond.models.ActivityType;
+import com.beyond.beyond.models.Period;
+import com.beyond.beyond.repositories.ActivityTypeRepository;
+import com.beyond.beyond.repositories.PeriodRepository;
+
+@Component
+public class ActivityMapper {
+    private ActivityTypeRepository activityTypeRepository;
+    private PeriodRepository periodRepository;
+
+
+    public ActivityMapper(
+        ActivityTypeRepository activityTypeRepository,
+        PeriodRepository periodRepository
+    ) {
+        this.activityTypeRepository = activityTypeRepository;
+        this.periodRepository = periodRepository;
+    }
+
+    public Activity toEntity(ActivityDTO dto) {
+        System.out.println(dto.toString());
+
+        ActivityType activityType = activityTypeRepository.findByType(dto.activityType()).orElseThrow(() -> new RuntimeException("Activity Type not found"));
+        Period period = periodRepository.findByName(dto.period()).orElseThrow(() -> new RuntimeException("Period not found"));
+
+        Activity activity = new Activity();
+        if (dto.id() != null) {
+            activity.setId(dto.id());
+        }
+        activity.setName(dto.name());
+        activity.setDescription(dto.description());
+        activity.setActivityType(activityType);
+        activity.setIsDefault(dto.isDefault());
+        activity.setPeriod(period);
+        activity.setPointsValue(dto.pointsValue());
+
+        return activity;
+    }
+
+    // ActivityDTO toDTO(Activity entity) {
+    //     //TODO
+    // }
+}
