e250021a786bd26e4166038ce3c338fe997bce8d
Caua Mattos <caua.h4k@gmail.com>
2025-11-18 22:47:27 -0300

    feat: improve activity dto

--- src/main/java/com/beyond/beyond/dto/ActivityDTO.java
+++ src/main/java/com/beyond/beyond/dto/ActivityDTO.java
@@ -7,13 +7,19 @@ import lombok.AllArgsConstructor;
 
 import java.util.UUID;
 
+import com.beyond.beyond.models.Activity;
+import com.beyond.beyond.models.ActivityType;
+import com.beyond.beyond.models.Period;
+
 public record ActivityDTO(
+    UUID id,
     @NotBlank String name,
     @NotBlank String description,
     @NotNull Integer pointsValue,
-    Boolean isDefault,
+    @NotNull Boolean isDefault,
     UUID userId,
-    UUID period,
-    UUID activityType
-) {}
+    @NotNull String period,
+    @NotNull String activityType
+) {
+}
 
