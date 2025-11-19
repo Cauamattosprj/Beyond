17468990f337d21e22c8f01b8e7be2d89494c597
Caua Mattos <caua.h4k@gmail.com>
2025-11-18 22:47:48 -0300

    feat: improve the activities controller to pass the test cases

--- src/main/java/com/beyond/beyond/controllers/ActivitiesController.java
+++ src/main/java/com/beyond/beyond/controllers/ActivitiesController.java
@@ -1,19 +1,56 @@
 package com.beyond.beyond.controllers;
 
-import org.springframework.stereotype.Controller;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beyond.beyond.dto.ActivityDTO;
+import com.beyond.beyond.mappers.ActivityMapper;
+import com.beyond.beyond.models.Activity;
+import com.beyond.beyond.repositories.ActivitiesRepository;
+import com.beyond.beyond.services.ActivitiesService;
 
 
 @RestController
+@RequestMapping("/api/activities")
 public class ActivitiesController {
-    @PostMapping("/activities")
-    public ActivityDTO postActivities(@RequestBody ActivityDTO activitie) {
-        
-        return activitie;
+    private final ActivitiesRepository activitiesRepository;
+    private final ActivitiesService activitiesService;
+    private final ActivityMapper activityMapper;
+    
+    public ActivitiesController(
+            ActivitiesRepository activitiesRepository,
+            ActivitiesService activitiesService,
+            ActivityMapper activityMapper
+    ) {
+        this.activitiesRepository = activitiesRepository;
+        this.activitiesService = activitiesService;
+        this.activityMapper = activityMapper;
     }
+
+    @PostMapping("")
+    public ResponseEntity<ActivityDTO> post(@RequestBody ActivityDTO dto) {
+
+        Activity activity = activityMapper.toEntity(dto);
+        Activity savedActivity = activitiesRepository.save(activity);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedActivity.toDto());
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Activity> getById(@PathVariable UUID id) {
+        Optional<Activity> activity = activitiesRepository.findById(id);
+        
+        return activity;
+    }
+    
     
 }
