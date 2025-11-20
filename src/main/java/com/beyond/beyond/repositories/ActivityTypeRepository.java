4196c674c74d3ba81c9d6e24e4072bac0994db04
Caua Mattos <caua.h4k@gmail.com>
2025-11-20 20:25:52 -0300

    fix: add repository annotation on repositories

--- src/main/java/com/beyond/beyond/repositories/ActivityTypeRepository.java
+++ src/main/java/com/beyond/beyond/repositories/ActivityTypeRepository.java
@@ -6,8 +6,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.beyond.beyond.models.ActivityType;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ActivityTypeRepository extends JpaRepository<ActivityType, UUID> {
     public Optional<ActivityType> findByType(String type);
 }
--- src/main/java/com/beyond/beyond/repositories/PeriodRepository.java
+++ src/main/java/com/beyond/beyond/repositories/PeriodRepository.java
@@ -5,10 +5,12 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.beyond.beyond.models.Period;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
-
+@Repository
 public interface PeriodRepository extends JpaRepository<Period, UUID>{
     Optional<Period> findByName(String name);
 }
