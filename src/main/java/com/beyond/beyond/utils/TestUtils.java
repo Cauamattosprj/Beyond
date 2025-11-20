43a582a9b787383c9f4ca8ee2996fa2264a2e7b3
Caua Mattos <caua.h4k@gmail.com>
2025-11-20 20:24:25 -0300

    feat: create test utils

---
+++ src/main/java/com/beyond/beyond/utils/TestUtils.java
@@ -0,0 +1,14 @@
+package com.beyond.beyond.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TestUtils {
+    public static String toJson(Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
